b4d88552740493046e506367497b864586fc1a90
Matthew Herzl <matthew.herzl@colorado.edu>
2015-06-03 11:38:43 -0600

    added variance

--- lib.js
+++ lib.js
@@ -1,23 +1,27 @@
-
 var bar = 'Hello!!';
 var x = bar;
-
 var sumAll = function(arr){
     return arr.reduce(function(a,e){return a + e;},0);
 };
-
 var productAll = function(arr){
     return arr.reduce(function(a,e){return a*e;},1);
 };
-
 var mean = function(arr){
     return sumAll(arr) / arr.length;
 };
-
+var squares = function(arr){
+    return arr.map(function(e){return e*e;});
+};
+var variance = function(arr){
+    var m = mean(arr);
+    return sumAll( squares( arr.map(function(e){return e-m;}) ) );
+};
 module.exports = {
     bar: bar,
     sumAll: sumAll,
     productAll: productAll,
-    mean: mean
+    mean: mean,
+    squares: squares,
+    variance: variance
 };
 
