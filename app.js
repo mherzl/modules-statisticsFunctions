5fb7245659c16ea89a3c48bf5ad0ca07ebcc02e4
Matthew Herzl <matthew.herzl@colorado.edu>
2015-06-03 11:18:32 -0600

    removed unnecessary commented cruft code

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var lib = require('./lib.js');
 var list = [1,2,3];
 
 var s = lib.sumAll(list);
-//var s = sumAll(list);
 var p = lib.productAll(list);
-//var p = productAll(list);
-//var e = lib.expAll(list);
 
 console.log(s);
 console.log(p);
